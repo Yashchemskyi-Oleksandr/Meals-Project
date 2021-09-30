585d2f211e7d43d4091c1130781c3ec45a1a3253
Oleksandr Yashchemskyi <yashchemskyio@gmail.com>
2021-09-30 12:18:36 +0300

    feat: implemented notification when deleted Meal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,6 @@
-import { categoriesReducer } from './store/categories/categories.reducer';
-import { mealsReducer } from './store/meals/meals.reducer';
-import { MealFormComponent } from './modules/admin/meal-form/meal-form.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { AppRoutingModule } from './app-routing.module';
-import { MealsComponent } from './modules/user/meals.component';
-import { MealsService } from './services/meals.service';
-import { CategoryService } from './services/category.service';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-
-import { InfoService } from './services/info.service';
-import { AdminComponent } from './modules/admin/admin.component';
-import { CategoriesPageComponent } from './modules/admin/categories-page/categories-page.component';
-import { CategoryFormComponent } from './modules/admin/category-form/category-form.component';
 import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { InfoPageComponent } from './modules/admin/info-page/info-page.component';
-import { infoReducer } from './store/info/info.reducer';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatInputModule } from '@angular/material/input';
@@ -31,6 +10,32 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  CategoriesPageComponent,
+  DialogUpdate,
+} from './modules/admin/categories-page/categories-page.component';
+import { infoReducer } from './store/info/info.reducer';
+import { mealsReducer } from './store/meals/meals.reducer';
+import { categoriesReducer } from './store/categories/categories.reducer';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MealFormComponent } from './modules/admin/meal-form/meal-form.component';
+import { MealsComponent } from './modules/user/meals.component';
+import { InfoPageComponent } from './modules/admin/info-page/info-page.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { AppComponent } from './app.component';
+
+import { CategoryService } from './services/category.service';
+import { MealsService } from './services/meals.service';
+import { InfoService } from './services/info.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -41,8 +46,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AdminComponent,
     MealFormComponent,
     CategoriesPageComponent,
-    CategoryFormComponent,
     InfoPageComponent,
+    DialogUpdate,
   ],
   imports: [
     BrowserModule,
@@ -65,6 +70,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSlideToggleModule,
     MatSelectModule,
     MatToolbarModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
   ],
   providers: [CategoryService, MealsService, InfoService],
   bootstrap: [AppComponent],
--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,27 +1,27 @@
-import { MealsService } from 'src/app/services/meals.service';
-import { applySearch, deleteMeal } from './../../store/meals/meals.action';
-import { AppState } from 'src/app/store/app.state';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Meals } from 'src/app/store/meals/meals.model';
 import { select, Store } from '@ngrx/store';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AppState } from 'src/app/store/app.state';
+import { Meals } from 'src/app/store/meals/meals.model';
+import { MealsService } from 'src/app/services/meals.service';
 import { selectMealsBySearch } from 'src/app/store/meals/meals.selector';
-import { Info } from 'src/app/store/info/info.model';
+import { applySearch, deleteMeal } from 'src/app/store/meals/meals.action';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
-  meals: Observable<Meals[]>;
+export class AdminComponent {
+  meals: Observable<Meals[]> = this.store.pipe(select(selectMealsBySearch));
 
   constructor(
     private mealsService: MealsService,
-    private store: Store<AppState>
-  ) {
-    this.meals = store.pipe(select(selectMealsBySearch));
-  }
+    private store: Store<AppState>,
+    private notificationService: NotificationService
+  ) {}
 
   search(query: string) {
     this.store.dispatch(applySearch({ search: query }));
@@ -29,11 +29,16 @@ export class AdminComponent implements OnInit {
 
   delete(id: string) {
     if (confirm('Are you sure you want to delete this product')) {
-      this.mealsService.deleteByIdMeal(id).subscribe((result) => {
-        this.store.dispatch(deleteMeal({ id: id }));
-      });
+      this.mealsService.deleteByIdMeal(id).subscribe(
+        (result) => {
+          this.store.dispatch(deleteMeal({ id: id }));
+          this.notificationService.success('Meal was deleted');
+        },
+        (error) => {
+          const { message } = error.error;
+          this.notificationService.error(message);
+        }
+      );
     }
   }
-
-  ngOnInit(): void {}
 }
