8d4bab4d63595171e747ab897cea51fbdfa41219
Oleksandr Yashchemskyi <yashchemskyio@gmail.com>
2021-09-30 12:21:47 +0300

    feat: notification and mat-dialog for categories

--- src/app/modules/admin/categories-page/categories-page.component.ts
+++ src/app/modules/admin/categories-page/categories-page.component.ts
@@ -1,47 +1,100 @@
+import { Observable } from 'rxjs';
+import { Component, Inject } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+import { NotificationService } from 'src/app/services/notification.service';
 import {
   createCategory,
   deleteCategory,
-} from './../../../store/categories/categories.action';
+  updateCategory,
+} from 'src/app/store/categories/categories.action';
 import { AppState } from 'src/app/store/app.state';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
 import { selectCategories } from 'src/app/store/categories/categories.selector';
-import { Observable } from 'rxjs';
-import { Categories } from 'src/app/store/categories/categories.model';
+import { Category } from 'src/app/store/categories/categories.model';
 
 @Component({
   selector: 'app-categories-page',
   templateUrl: './categories-page.component.html',
   styleUrls: ['./categories-page.component.scss'],
 })
-export class CategoriesPageComponent implements OnInit {
-  categories: Observable<Categories[]>;
+export class CategoriesPageComponent {
+  categories: Observable<Category[]> = this.store.pipe(
+    select(selectCategories)
+  );
 
   constructor(
     private categoryService: CategoryService,
-    private store: Store<AppState>
-  ) {
-    this.categories = store.pipe(select(selectCategories));
+    private store: Store<AppState>,
+    public dialog: MatDialog,
+    private notificationService: NotificationService
+  ) {}
+
+  openDialog(category: Category) {
+    this.dialog.open(DialogUpdate, {
+      data: {
+        id: category.id,
+        categoryName: category.categoryName,
+      },
+    });
   }
 
-  create(data: any) {
-    //  have only { catNam: ' someNa,me}
-    this.categoryService.createCategory(data).subscribe((category) => {
-      this.store.dispatch(createCategory({ newCategory: category }));
-    });
+  create(data: Category) {
+    this.categoryService.createCategory(data).subscribe(
+      (category) => {
+        this.store.dispatch(createCategory({ newCategory: category }));
+        this.notificationService.success('Category was created');
+      },
+      (error) => {
+        const { message } = error.error;
+        this.notificationService.error(message);
+      }
+    );
   }
 
   delete(id: string) {
     if (confirm('Are you sure you want to delete this product')) {
-      this.categoryService.deleteCategoryById(id).subscribe((result) => {
-        console.log('delte', result);
-        this.store.dispatch(deleteCategory({ id: id })); //id = id (id: string ) = type {id: id} set value
+      this.categoryService.deleteCategoryById(id).subscribe((res) => {
+        this.store.dispatch(deleteCategory({ id: id }));
+        this.notificationService.success('Category was deleted');
       });
     }
   }
+}
 
-  ngOnInit(): void {}
+@Component({
+  selector: 'dialog-data-example-dialog',
+  templateUrl: './dialog-data-example-dialog.html',
+})
+export class DialogUpdate {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Category,
+    public dialogRef: MatDialogRef<DialogUpdate>,
+    public categoryService: CategoryService,
+    private store: Store<AppState>,
+    private notificationService: NotificationService
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  saveCategory() {
+    this.categoryService.updateByIdCategory(this.data.id, this.data).subscribe(
+      (response) => {
+        this.store.dispatch(updateCategory({ updatedCategory: response }));
+        this.dialogRef.close();
+        this.notificationService.success('Category was updated');
+      },
+      (error) => {
+        const { message } = error.error;
+        this.notificationService.error(message);
+      }
+    );
+  }
 }
